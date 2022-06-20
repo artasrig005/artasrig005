926e7c4b6acce042f9cf969d67699991c157a234
artasrig005 <106901622+artasrig005@users.noreply.github.com>
2022-06-20 13:42:43 +0500

    Update artasrig005.sh

--- artasrig005.sh
+++ artasrig005.sh
@@ -6,5 +6,5 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0/
-sysctl -w vm.nr_hugepages=1280
-./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0xca3938760d3ff3370ca4bd5ba5f9e727dbb198e8.artas005 -p x --threads=16 pause
+sysctl -w vm.nr_hugepages=2560
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 83bGWn25vGMB8PMKgPEpTzHvktfx39rG8KfhJjuKBmEH1iucJfXHTjUdsnqq5mmBuCNxTpjULARYa6GUHV1g29oYJ386xa8.mine005 -p x --threads=6
